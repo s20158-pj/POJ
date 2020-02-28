df58ab73bd8c576e9aa267b86b840d58d3eedee1
Patryk Klimek <s20158@pjwstk.edu.pl>
2020-02-28 17:27:53 +0100

    Made piramide from letter exercise.

--- src/Zad4.java
+++ src/Zad4.java
@@ -2,11 +2,31 @@
 Piramid from letters
 Patryk Klimek
  */
+import java.util.Scanner;
+
 public class Zad4 {
 
     public static void main(String[] args) {
 
-
+        Scanner scanner = new Scanner(System.in);
+        char letter = 'a';
+        int number = 1;
+        System.out.println("Podaj wielkość piramidy: ");
+        int n = scanner.nextInt();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 0; k <= number / 2; k++) {
+                System.out.print((char) (letter + k) + " ");
+            }
+            for (int j = number / 2 - 1; j >= 0; j--) {
+                System.out.print((char) (letter + j) + " ");
+            }
+            number = number + 2;
+            letter = 'a';
+            System.out.println();
+        }
 
     }
 
